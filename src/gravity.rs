132dc5ec057f63ef9f78643bb499200eb056b2d3
Anerdw <yimandrew27@gmail.com>
2026-07-07 02:28:19 -0500

    feat: add trivial forwards to GravityGame

--- src/gravity.rs
+++ src/gravity.rs
@@ -1,5 +1,33 @@
-use crate::MnkGame;
+use crate::{GameStatus, MnkGame};
+use std::fmt;
 
 /// A restricted [`MnkGame`] where stones must be placed at the bottom of a column.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub struct GravityGame<const R: usize, const C: usize, const K: usize>(MnkGame<R, C, K>);
+
+impl<const R: usize, const C: usize, const K: usize> GravityGame<R, C, K> {
+    /// Returns a [`GameStatus::Ongoing`] `GravityGame<R, C, K>` with an empty board and current
+    /// player [`Player::X`].
+    #[must_use]
+    pub fn new() -> Self {
+        Self(MnkGame::new())
+    }
+
+    /// The current [`GameStatus`] of the game.
+    #[must_use]
+    pub const fn status(&self) -> GameStatus {
+        self.0.status()
+    }
+}
+
+impl<const R: usize, const C: usize, const K: usize> Default for GravityGame<R, C, K> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<const R: usize, const C: usize, const K: usize> fmt::Display for GravityGame<R, C, K> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
